bc946adaac5787b988887f4131971202b6f81ab3
PrgDominique <dominiquesimbillo10@gmail.com>
2022-10-10 09:50:49 +0800

    change currency sign

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,7 +10,7 @@ function ExpenseItem(props) {
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">₱{props.amount}</div>
       </div> 
     </Card>
     </li>
